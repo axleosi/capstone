6278e4a5d403ce99d7eb7db4da6e13d0b97290b9
axleosi <axleosi@gmail.com>
2024-06-28 11:36:32 +0100

    changes made to the design of the website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import Header from './Header';
-import Nav from './Nav';
 import Main from './Main';
 import Footer from './Footer';
 
 function App() {
   return (
     <>
-    <Header></Header>
-    <Nav></Nav>
-    <Main></Main>
-    <Footer></Footer>
+    <Header/>
+    <Main/>
+    <Footer/>
     </>
   );
 }
--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,9 +1,24 @@
 import React from 'react'
 function Header(){
     return(
-        <header>
-            <img src="./images/logo1.jpg"></img>
+    <>
+        <header class="hcon">
+            <div>
+            <img src="./images/logo1.jpg" height="50px" width="200px"></img>
+            </div>
+            <div>
+            <nav>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a >Menu</a></li>
+                <li><a>Reservations</a></li>
+                <li><a>Order Online</a></li>
+            </ul>
+        </nav>
+            </div>
         </header>
+        </>
     )
 }
 
